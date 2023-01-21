29a842ea9ea0a049ac3b844203a99dd45e0facca
jaugs <johnaugsburger@gmail.com>
2023-01-20 19:18:23 -0700

    started animation component testing

---
+++ src/components/animate.js
@@ -0,0 +1,75 @@
+import { useState, useRef, useEffect } from "react";
+
+const useElementOnScreen = (options) => {
+    const containerRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false)
+
+    const makeAppear = (entries) => {
+        const [entry] = entries;
+        if (entry.isIntersecting)
+          setIsVisible(true);
+      };
+
+      const makeAppearRepeating = (entries) => {
+        const [entry] = entries;
+        setIsVisible(entry.isIntersecting);
+      };
+
+      const callBack = options.reappear ? makeAppearRepeating : makeAppear;
+
+      useEffect(() => {
+        const containerRefCurrent = containerRef.current
+        const observer = new IntersectionObserver(callBack, options);
+        if (containerRefCurrent)
+          observer.observe(containerRefCurrent);
+    
+        return () => {
+          if (containerRefCurrent) {
+            observer.unobserve(containerRefCurrent);
+          }
+        };
+      }, [containerRef, options, callBack]);
+    
+      return [containerRef, isVisible];
+    };
+  
+
+
+
+const AnimateOnScroll = (props) => {
+
+    const { children, reappear, threshold  } = props
+
+    const visibleStyle = {
+        transitionDuration: '1000',
+        opacity: '100',
+        blur: 'none',
+        color: 'red',
+       // transform: 'translate(40px, 100px)'
+    }
+
+    const notVisibleStyle = {
+        color: 'blue',
+        transitionDuration: '1000',
+        opacity: '0',
+        blur: 'lg',
+       transform: 'translate(40px, 100px)'
+    }
+
+
+
+    const [containerRef, isVisible] = useElementOnScreen({
+      threshold: threshold,
+      reappear: reappear,
+    });
+  
+    return (
+      <>
+        <div ref={containerRef} className='newDiv' style={isVisible ? visibleStyle : notVisibleStyle}>
+          {children}
+        </div>
+      </>
+    );
+  }
+  export default AnimateOnScroll
+  //{`transition duration-1000 ${isVisible ? "opacity-100 blur-none translate-x-0" : "opacity-0 blur-lg -translate-x-20"}  motion-reduce:transition-none motion-reduce:hover:transform-none`}
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,19 +1,30 @@
 import '../styles/homepageStyle.css'
 import storePic from '../images/store.png'
 import storePic2 from '../images/store2.png'
+//import { useEffect, useRef, useState } from "react";
+import AnimateOnScroll from './animate'
 
 export default function Homepage (props) {
 
+   // let observer = new IntersectionObserver(callback, options);
 
+    // const options = {
+    //     threshold: 0,
+    //     reappear: false,
+    //   }
+      
+     
     return (
         <div className='homepage'>
             <div className='firstSection'>
                 <img className='shopImage' src= {storePic} alt='store display' />
+                <div className='descriptionContainer'>
                 <div className='description'>
                     Welcome to Otherwides Wide Shoes, we are an independent full-service shoe store that specializes in wide and extra-wide width shoes for men and women. 
                     We have a huge selection of in-stock footwear, these include dress, casual, sandals, boots, work and athletic shoes. 
                     From properly measuring your feet, fitting orthotics, finding comfortable work shoes, 
                     getting fit in a top-of-the-line running shoe to finding the perfect evening shoe for that special night out, our experienced staff can help.
+                </div>
                 </div>
             </div>
             <div className='secondSection'>
@@ -33,6 +44,7 @@ export default function Homepage (props) {
                  </div>
                  <img className='shopImage' src= {storePic2} alt='second store display' />
             </div>
+            <AnimateOnScroll children={<div>ddd</div>} reappear={false} threshold={.9} />
         </div>
     )
 }
