4a8619f57be0c7451bc406a2fcac1d4b7ff326b7
jaugs <johnaugsburger@gmail.com>
2023-02-13 16:57:38 -0700

    changed map

--- src/components/createMap.js
+++ src/components/createMap.js
@@ -1,30 +1,34 @@
 //import { Wrapper, Status } from '@googlemaps/react-wrapper'
-//import { useEffect, useRef, useState } from 'react'
-import mapPic from '../images/storemap.png'
+import { useEffect, useRef, useState } from 'react'
+//import mapPic from '../images/storemap.png'
 
 export default function MapMarker() {
 
-    // const ref = useRef(null)
-    // const [map, setMap] = useState();
-    // useEffect(() => {
-    //     if (ref.current && !map) {
-    //         setMap(new Window.google.maps.Map(ref.current, {}));
-    //     }
+    const key = process.env.REACT_APP_API_KEY
 
-    // }, [ref, map]);
+    const ref = useRef(null)
+
+    const [map, setMap] = useState();
+
+    useEffect(() => {
+        if (ref.current && !map) {
+            setMap(new Window.google.maps.Map(ref.current, {}));
+        }
+
+    }, [ref, map]);
 
     return (
         <div className='mapContainer'>
-            <img className='storePic' src={mapPic} alt='map to store'></img>
-            {/* <iframe
+            {/* <img className='storePic' src={mapPic} alt='map to store'></img> */}
+            <iframe
                 title='map'
                 width="450"
                 height="250"
                 
                 referrerPolicy="no-referrer-when-downgrade"
-                src="https://www.google.com/maps/embed/v1/place?key=YOURKEY&q=Otherwides+Shoes,Tempe+AZ"
-                allowfullscreen>
-            </iframe> */}
+                src= {`https://www.google.com/maps/embed/v1/place?key=${key}&q=Otherwides+Shoes,Tempe+AZ`}
+                allowFullScreen>
+            </iframe>
         </div>
     )
 }
