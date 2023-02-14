7e7ae09b980537c890f1a68e582f4f2580601fdf
jaugs <johnaugsburger@gmail.com>
2023-02-14 13:41:14 -0700

    updated maps

--- src/components/createMap.js
+++ src/components/createMap.js
@@ -10,6 +10,52 @@ export default function MapMarker() {
 
     const [map, setMap] = useState();
 
+
+    // const [todo, setTodo] = useState('');
+    // const [todoId, setTodoId] = useState(1);
+
+    // async function getNewTodo() {
+    //     const response = await fetch('/.netlify/functions/hello-world')
+    //           .then(response => response.json()
+    //       )
+      
+    //      await console.log(response)
+    //   }
+
+    const mapStyle = {
+        border: 'none',
+        borderRadius: '15px',
+        width: '25rem',
+        height: '25rem'
+    }
+
+    // useEffect(() => {
+    //     async function fetchTodo() {
+    //       const url = `../../netlify/functions/todo?id=1`;
+    //       console.log(url)
+    //       try {
+    //         const todo = await fetch(url)
+    //         const data = todo.json()
+    //         console.log(data)
+    //         setTodo(todo.title);
+    //       } catch (err) {
+    //         console.log(err);
+    //       }
+    //     }
+    //     fetchTodo();
+    //   }, [todoId]);
+    
+    // exports.handler = async function (event, context) {
+    //     const value = process.env.REACT_APP_API_KEY;
+      
+    //     return {
+    //       statusCode: 200,
+    //       body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${value}.` }),
+    //     };  
+    //   };
+
+
+
     useEffect(() => {
         if (ref.current && !map) {
             setMap(new Window.google.maps.Map(ref.current, {}));
@@ -19,12 +65,11 @@ export default function MapMarker() {
 
     return (
         <div className='mapContainer'>
+           {/* <button onClick={getNewTodo}> Get another todo </button> */}
             {/* <img className='storePic' src={mapPic} alt='map to store'></img> */}
             <iframe
                 title='map'
-                width="450"
-                height="250"
-                
+                style= {mapStyle}
                 referrerPolicy="no-referrer-when-downgrade"
                 src= {`https://www.google.com/maps/embed/v1/place?key=${key}&q=Otherwides+Shoes,Tempe+AZ`}
                 allowFullScreen>
