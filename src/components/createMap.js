85ae6bc579276e4f1dfb4858715c86ae6b564d49
jaugs <johnaugsburger@gmail.com>
2023-02-15 11:47:45 -0700

    fixing maps

--- src/components/createMap.js
+++ src/components/createMap.js
@@ -1,37 +1,14 @@
 //import { Wrapper, Status } from '@googlemaps/react-wrapper'
-import { useEffect, useRef, useState } from 'react'
 //import mapPic from '../images/storemap.png'
 
 export default function MapMarker() {
 
-    let serverURL;
-
-    fetch("../../netlify/functions/todo")
-    .then(response => response.json())
-    .then(json => {
-        serverURL = json.api;
-    })
-
-    const ref = useRef(null)
-
-    const [map, setMap] = useState();
-
-    
     const mapStyle = {
         border: 'none',
         borderRadius: '15px',
         width: '25rem',
         height: '25rem'
     }
-
-
-
-    useEffect(() => {
-        if (ref.current && !map) {
-            setMap(new Window.google.maps.Map(ref.current, {}));
-        }
-
-    }, [ref, map]);
 
     return (
         <div className='mapContainer'>
@@ -40,9 +17,10 @@ export default function MapMarker() {
                 title='map'
                 style= {mapStyle}
                 referrerPolicy="no-referrer-when-downgrade"
-                src= {`https://www.google.com/maps/embed/v1/place?key=${serverURL}&q=Otherwides+Shoes,Tempe+AZ`}
+                src= {`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_API_KEY}&q=Otherwides+Shoes,Tempe+AZ`}
                 allowFullScreen>
             </iframe>
         </div>
     )
 }
+// `https://www.google.com/maps/embed/v1/place?key=${}&q=Otherwides+Shoes,Tempe+AZ`
