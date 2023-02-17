a033098eb8df2c2fe1feeceff1bc24ca561fe7de
jaugs <johnaugsburger@gmail.com>
2023-02-17 16:35:38 -0700

    form work

--- src/components/signup.js
+++ src/components/signup.js
@@ -2,8 +2,8 @@
 export default function SignUpForm() {
     return(
         <div className="signupContainer">
-            <form method="POST" name="emailSignUp" data-netlify='true' netlify-honeypot="bot-field">
-                <input name="bot-field"></input>
+            <form method="POST" name="email">
+                <input type="hidden" name="form-name" value="emailForm" />
                 <label>Sign Up with your Email to recieive exclusive offers, deals and more!</label>
                 <input type='email' name='email' className='emailInput'></input>
                 <input className='submitButton' type='submit' value='Submit'></input>
